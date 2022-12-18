57ecce8ef1b8a3e07b5992268c738257f0c93b99
Guy Mograbi <guy.mograbi@gmail.com>
2022-12-18 09:53:07 -0800

    task: add lambda example

--- README.md
+++ README.md
@@ -35,6 +35,41 @@ Everything I need to know is in the [hashicorp registery](https://registry.terra
 1. DO NOT manage the statefile yourself. (For example: DO NOT keep the statefile in git)
 2. DO let terraform manage the statefile (using `backend` resource [link](https://developer.hashicorp.com/terraform/language/settings/backends/configuration))
 
+## Structure
+
+```
+resource "[TYPE]" "[NAME]" {
+    [ATTRIBUTE] = "[VALUE]"
+}
+```
+
+
+```
+variable "[variable name]" {
+    default     = "[optional]"
+    description = "[optional]"
+    type        = "string|int|list|map"
+}
+```
+
+```
+data "[TYPE]" "[NAME]" {}
+```
+
+
+**variables values**
+Terraform loads all files that match terraform.tfvars or *.auto.tfvars present in the current directory.
+
+The format of this is simply
+
+```
+[key] = [value]
+```
+
 # Resources
 
-https://medium.com/mlearning-ai/terraform-to-provision-aws-resource-s3-bucket-beginner-level-5dde3a8afdc4
+1. https://medium.com/mlearning-ai/terraform-to-provision-aws-resource-s3-bucket-beginner-level-5dde3a8afdc4
+2. https://aws.amazon.com/blogs/database/managing-amazon-elasticache-with-terraform/ 
+3. https://www.youtube.com/watch?v=Lkm3v7UDlD8
+4. https://awspolicygen.s3.amazonaws.com/policygen.html
+5. https://registry.terraform.io/providers/hashicorp/aws/2.34.0/docs/guides/serverless-with-aws-lambda-and-api-gateway
---
+++ lambda-demo/lambda.tf
@@ -0,0 +1,78 @@
+
+resource "aws_iam_role_policy" "lambda_policy" {
+   name = "lambda_policy"
+   role = "${aws_iam_role.lambda_role.id}"
+
+   policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "",
+            "Action": [
+                "logs:*"
+            ],
+            "Effect": "Allow",
+            "Resource": "*"
+
+        }
+    ]
+}
+   EOF
+}
+
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_assume_role"
+
+
+# alternatively assume_role_policy = "${file("path/to/file")}"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+locals {
+    lambda_zip_location = "output/welcome.zip"
+}
+
+# data "archive_file" "welcome" {
+#     type = "zip"
+#     source_file = "welcome.py"
+#     output_path = "${local.lambda_zip_location}"
+# }
+
+resource "aws_lambda_function" "test_lambda" {
+    filename = "${local.lambda_zip_location}"
+    function_name = "welcome"
+    role = aws_iam_role.lambda_role.arn
+    handler = "welcome.hello"
+
+    # This is used to identify changes in the code - which will trigger terraform to redeploy
+    source_code_hash = "${filebase64sha256(local.lambda_zip_location)}"
+
+    runtime = "python3.7"
+
+    environment {
+        variables = {
+            "mograbious": "wonderful"
+        }
+    }
+}
+
+
+output "terraform_aws_role_output" {
+    value = aws_iam_role.lambda_role.name
+}
